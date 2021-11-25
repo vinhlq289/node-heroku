3bc68a9caad45462c741475c576733c506f1d87b
VINHLQBK <lequangvinh2891996@gmail.com>
2021-11-25 22:39:23 +0700

    update

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${process.env.PORT}`));
